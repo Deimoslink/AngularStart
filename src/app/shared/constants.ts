89e78e5b50151d83a3075f8c18ee5783a8a2e273
Aleksandr Linkov <aleksandr_linkov@epam.com>
2018-06-27 18:47:12 +0400

    parts of speech changed

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,2 +1,2 @@
 export const COLORS = ['#90bc24', '#47c0ca', '#ed5564', '#f6bb43', '#d558e0', '#f8997f'];
-export const PARTS_OF_SPEECH = ['pronoun', 'noun', 'verb', 'adjective', 'adverb', 'subordinate', 'preposition'];
+export const PARTS_OF_SPEECH = ['noun', 'verb', 'adj', 'adv', 'misc'];
